40acce7070d55e4d5facdcbdd4bd5c095cd5639d
Karla Ramirez <karlar1824@gmail.com>
2021-02-07 22:15:06 -0500

    save scene

--- Assets/Scripts/OrderMovement.cs
+++ Assets/Scripts/OrderMovement.cs
@@ -7,6 +7,7 @@ public class OrderMovement : MonoBehaviour{
     public GameObject Customer;
     public Vector3 startPos;
     public Vector3 speed;
+    // when it gets to a certain point thje G.o stops 
     public Vector3 cutOff;
     public float maxTime;
     public float minTime;
@@ -18,7 +19,7 @@ public class OrderMovement : MonoBehaviour{
 
     // Update is called once per frame
     void Update()
-    {
+    {   // if it reachs a certain x it will go somewhere
         if (GetComponent<Transform>().position.x < cutOff.x) {
             // yield return new WaitForSeconds(spawnTime);
             transform.position += speed;
@@ -28,7 +29,7 @@ public class OrderMovement : MonoBehaviour{
             OrderBehavior.orderComplete = false;
         }
     }
-
+    // spawn randomlys 
     void RandomTime() {
         spawnTime = Random.Range (minTime, maxTime);
     }
